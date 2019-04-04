c260b33f3ddd6ca8f4c8f8412e3dd2599b2e3c02
yusure <czhyusure@163.com>
2019-04-04 09:02:39 +0800

    Format date display

--- web/usr/themes/Baroque-Normal/post.php
+++ web/usr/themes/Baroque-Normal/post.php
@@ -5,7 +5,7 @@
     <article class="post" itemscope itemtype="http://schema.org/BlogPosting">
         <h1 class="post-title" itemprop="name headline"><?php $this->title() ?></h1>
         <div class="post-meta">
-            <span class="post-category"><?php $this->category(','); ?></span><span datetime="<?php $this->date('c'); ?>" itemprop="datePublished"><?php $this->date('n月j日'); ?></span><span><?php $this->commentsNum('暂无评论', '1 条评论', '%d 条评论'); ?></span>
+            <span class="post-category"><?php $this->category(','); ?></span><span datetime="<?php $this->date('c'); ?>" itemprop="datePublished"><?php $this->date('Y-m-d'); ?></span><span><?php $this->commentsNum('暂无评论', '1 条评论', '%d 条评论'); ?></span>
         </div>
         <div class="post-content" itemprop="articleBody">
             <?php $this->content(); ?>
